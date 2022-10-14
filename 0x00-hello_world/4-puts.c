77f84bb4f5a2619271724903864f116c36cad2b7
Kornebari <tambarikornebari@gmail.com>
2022-10-14 10:10:32 -0700

    Programming is fun

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+
 /**
  * main - Entry
- * Return: Always 0
+ *
+ * Return: Always 0 (success)
  */
-int mainmain(void)
+
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
 }

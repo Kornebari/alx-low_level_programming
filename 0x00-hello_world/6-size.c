0fb6db839ac753a8da92883955382d3d7d477091
Kornebari <tambarikornebari@gmail.com>
2022-10-15 09:58:33 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,6 +9,12 @@
 
 int main(void)
 {
+	char a;
+	int i;
+	long int l;
+	long long int b;
+	float f;
+
 	printf("Size of char: %lu bytes\n", sizeof(char));
 	printf("Size of int: %lu bytes\n", sizeof(int));
 	printf("Size of long int: %lu bytes\n", sizeof(long int));

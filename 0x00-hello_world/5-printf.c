b7cbda02844631b8dbca2bc739fd9f1bb5a97674
Kornebari <tambarikornebari@gmail.com>
2022-10-15 20:29:18 -0700

    Task 5 corrected

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * Main-Entry
+ * Main- Entry point
  *
  * Return: Always 0 (success)
  *
  */
-
 int main(void)
 
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+
 	return (0);
 }

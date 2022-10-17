8424321675d5e06c773afd23d9ba0a5dfb7da1d3
Kornebari <tambarikornebari@gmail.com>
2022-10-17 07:44:51 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,18 +12,12 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-       	{
-	printf("n is positive\n");
-	} 
-	else if (n == 0)
-       	{
-	printf("n is 0\n");
-	}
+	if (n > 0);
+	printf("%d is positive\n", n);
+	else if (n == 0);
+	printf("%d is zero\n", n);
       	else if (n < 0);
-       	{
-	printf("n is negative\n");
-	} 
+	printf("%d is negative\n", n);
 	return (0);
 }
 

9746dd3082fd14252ec5fda0e170480d510e2144
Kornebari <tambarikornebari@gmail.com>
2022-10-17 20:26:58 -0700

    Task1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,6 +7,7 @@
  *
  * Return: Always 0 (success)
  *
+ * p stores the modolus of n
  */
 
 int main(void)
@@ -18,15 +19,15 @@ int main(void)
 	p = n % 10;
 	if (p > 5)
 	{
-		printf("The last digit of %d is %d and is greater than 5\n", n, p);
+		printf("The last digit of %d is %d and is greater than 5\n");
 	}
 			else if (p == 0)
 			{
-				printf("The last digit of %d is %d and is 0\n", n, p);
+				printf("The last digit of %d is %d and is 0\n");
 			}
 	else if ((p < 6) && (p < 0))
 	{
-		printf("The last digit of %d is %d and is less than 6 and not 0\n", n, p);
+		printf("The last digit of %d is %d and is less than 6 and not 0\n");
 	}
 	return (0);
 }

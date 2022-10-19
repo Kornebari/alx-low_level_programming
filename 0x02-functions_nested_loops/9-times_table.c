2dd27576de624701aec050d0e896949c98414a58
Kornebari <tambarikornebari@gmail.com>
2022-10-18 20:42:29 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,7 +10,7 @@ for (num = 0; num <= 9; num++)
 {
 _putchar('0');
 
-for (mult = 0; mult <= 9; mult++)
+for (mult = 1; mult <= 9; mult++)
 {
 _putchar(',');
 _putchar(' ');
@@ -19,7 +19,7 @@ if (prod <= 9)
 _putchar(' ');
 else
 _putchar((prod / 10) + '0');
-_putchar((prod % 10) + '0');;
+_putchar((prod % 10) + '0');
 }
 _putchar('\n');
 }

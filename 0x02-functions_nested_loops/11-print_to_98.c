dee60fc217e974596fe3aac3d256e55fc0266403
Kornebari <tambarikornebari@gmail.com>
2022-10-18 21:15:24 -0700

    Task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * print_to_98 - prints natural numbers from n to 98
+ * @n: The first natural number
+ */
+
+void print_to_98(int n)
+{
+if (n >= 98)
+{
+while (n >= 98)
+printf("%d ,", n--);
+printf("%\n", n);
+}
+else
+{
+	while (n < 98)
+		printf("%d ", n++);
+	printf("\n", n);
+}
+}
+

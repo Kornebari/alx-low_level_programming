5663575b3b9dd21d1bd9e10548138dcff7e97d0f
Kornebari <tambarikornebari@gmail.com>
2022-10-28 16:13:38 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,19 +2,20 @@
 
 /**
  * more_numbers - Print more numbers
+ * return: no return
  */
 void more_numbers(void)
 {
 	int a, b;
 
-	for (a = 0; a <= 10; a++)
-	{
-	for (b = 0; b <= 14; b++)
-	{
-	if (b >= 10)
-	_putchar('1');
-_putchar (b % 10 + '0');
-	}
-	_putchar('\n');
-	}
+		for (a = 1; a <= 10; a++)
+		{
+			for (b = 0; b <= 14; b++)
+			{
+				if (b >= 10)
+				_putchar('1');
+				_putchar (b % 10 + '0');
+			}
+				_putchar('\n');
+		}
 }

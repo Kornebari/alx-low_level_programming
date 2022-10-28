8ec82a9567b02d50dc1a279c8651e37622cd071f
Kornebari <tambarikornebari@gmail.com>
2022-10-28 15:56:21 -0700

    t6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,15 +3,15 @@
 /**
  * print_line - Prints a straight line with '_'
  * @n: The number of '_' to be printed
+ * return: void
  */
 void print_line(int n)
 {
 	int len;
 
-	if (n > 0)
+	for (len = 0; len < n; len++)
 	{
-		for (len = 0; len < n; len++)
-			_putchar('_');
+		_putchar('_');
 	}
 	_putchar(''\n');
 }

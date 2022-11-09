9890f3ee4e9bdf2493e0fd9107edd05b5f137915
Kornebari <tambarikornebari@gmail.com>
2022-11-08 16:36:05 -0800

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,8 +13,8 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	if (size == 0)
-		return(NULL);
-	array = (char *)malloc(sizeof(char) *size);
+		return (NULL);
+	array = (char *)malloc(sizeof(char) * size);
 	if (array == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)

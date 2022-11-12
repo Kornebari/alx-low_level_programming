38ccdfa7bcf19b70ca5b0a02ca233073cf6c93b7
Kornebari <tambarikornebari@gmail.com>
2022-11-11 20:06:06 -0800

    Task 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -13,10 +13,10 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 		size = max - min + 1;
-		ptr = malloc(sizeof(int * size);
+		ptr = malloc(sizeof(int) * size);
 				if (ptr == NULL)
 				return (NULL);
 				for (i = 0; min <= max; i++)
 				ptr[i] = min++;
 				return (ptr);
-				}
+}

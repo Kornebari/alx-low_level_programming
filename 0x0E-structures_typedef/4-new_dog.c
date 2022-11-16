1fcfe6a5e4a3307c136d716f5d41d6df8ebf6055
Kornebari <tambarikornebari@gmail.com>
2022-11-16 01:13:20 -0800

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,18 +14,19 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
-	dog = malloc(ni *sizeof(char)):
+	dog = malloc(ni * sizeof(char));
 		if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
 	for (i = 0; i < ni; i++)
-		dog->name[i]= name[i];
+		dog->name[i] = name[i];
 	dog->age = age;
-	for (ol =0; owner[ol]; ol++);
+	for (ol = 0; owner[ol]; ol++)
+		;
 	ol++;
-	dog->owner = malloc(ol *sizeof(char));
+	dog->owner = malloc(ol * sizeof(char));
 	if (dog->owner == NULL)
 	{
 		free(dog->name);

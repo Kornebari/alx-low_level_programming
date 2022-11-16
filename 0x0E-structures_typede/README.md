18dda46b6c629c3367e88f354188202f7b6d24fa
Kornebari <tambarikornebari@gmail.com>
2022-11-15 20:43:29 -0800

    struct dog

---
+++ 0x0E-structures_typede/README.md
@@ -0,0 +1,7 @@
+Structures are user defined date type in c that allows you to combine data items of different.
+
+The different files in this directory contain codes on  the type struct dog. 
+
+Take time to go through them to fully understand.
+
+All the best fron Kornebari
---
+++ 0x0E-structures_typede/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - dog class
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+}
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *);
+typedef struct dog dog_t
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif

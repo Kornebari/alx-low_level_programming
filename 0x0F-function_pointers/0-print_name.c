eff8695f93648a84049c8a32379dad1dff0987da
Kornebari <tambarikornebari@gmail.com>
2022-11-19 21:57:07 -0800

    task0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,4 @@
-#include "functions_pointers.h"
+#include "function_pointers.h"
 /**
  * print_name - a function that prints a name.
  * @name: the name given
@@ -7,6 +7,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f !== NULL)
+	if (name != NULL && f != NULL)
 		f(name);
 }
